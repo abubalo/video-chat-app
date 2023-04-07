39a155b5bb4fb4809cd0bb17b73814d997e5d227
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-04-07 23:50:14 +0200

    Modified

--- js/room.js
+++ js/room.js
@@ -1,74 +1,65 @@
-function switchTab(){
-  chatroomBtn = document.querySelector('#chatroom-btn');
-  participantBtn = document.querySelector('#parti-btn');
+function switchTab() {
+  chatroomBtn = document.querySelector("#chatroom-btn");
+  participantBtn = document.querySelector("#parti-btn");
 
-  chatPanel = document.querySelector(`#parti-panel[data-id=${1}]`)
-  ParticipantPanel = document.querySelector('#parti-panel')
+  chatPanel = document.querySelector(`#parti-panel[data-id=${1}]`);
+  ParticipantPanel = document.querySelector("#parti-panel");
 
-  chatroomBtn.addEventListener('click', ()=>{
-    console.log('hello there');
-  })
+  chatroomBtn.addEventListener("click", () => {
+    console.log("hello there");
+  });
 
-  participantBtn.addEventListener('click', ()=>{
-    console.log('working')
-  })
-
-  
+  participantBtn.addEventListener("click", () => {
+    console.log("working");
+  });
 }
 
+function handleChat() {}
 
-function handleChat(){
-  
-
-
-
+async function handleControls() {
+  const mic = document.querySelector("#mic");
+  const camera = document.querySelector("#camera");
+  const screen = document.querySelector("#screen");
 }
 
-
-async function handleControls(){
-  const mic = document.querySelector('#mic')
-  const camera = document.querySelector('#camera')
-  const screen = document.querySelector('#screen');
-
-
-
-}
-
-
-async function leaveChannell(){
-  // const 
+async function leaveChannell() {
+  // const
 }
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
 
-function updateTimer(){
-    seconds++;
-    if (seconds === 60) {
-      seconds = 0;
-      minutes++;
-    }
-    if (minutes === 60) {
-      minutes = 0;
-      hours++;
-    }
-    document.getElementById("seconds").innerHTML = formatTime(seconds);
-    document.getElementById("minutes").innerHTML = formatTime(minutes);
-    document.getElementById("hours").innerHTML = formatTime(hours);
+function updateTimer() {
+  seconds++;
+  if (seconds === 60) {
+    seconds = 0;
+    minutes++;
   }
-
+  if (minutes === 60) {
+    minutes = 0;
+    hours++;
+  }
+  document.getElementById("seconds").innerHTML = formatTime(seconds);
+  document.getElementById("minutes").innerHTML = formatTime(minutes);
+  document.getElementById("hours").innerHTML = formatTime(hours);
+}
 
 function formatTime(time) {
-    return time < 10 ? "0" + time : time;
+  return time < 10 ? "0" + time : time;
 }
 
 setInterval(() => {
   updateTimer();
 }, 1000);
 
-
-
-
 const mainframe = document.querySelector("#main--screen");
-const participant = document.querySelector("#participants");
+const participants = document.querySelector(".users");
 
+function expandVideoFrame() {
+  for (let i = 0; i < participants.length; i++) {
+    const participant = participants[i];
+    participant.addEventListener("click", () => {
+      mainframe.textContent = "Video active";
+    });
+  }
+}
